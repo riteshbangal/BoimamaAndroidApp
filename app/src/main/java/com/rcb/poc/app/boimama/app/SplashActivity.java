a8f6e5608a374ff1a4e1512fd3aa93b87853dc43
Ritesh Bangal <riteshbangal@gmail.com>
2024-02-24 19:45:50 +0100

    Implement Splash Screen without logo

--- app/src/main/java/com/rcb/poc/app/boimama/app/SplashActivity.java
+++ app/src/main/java/com/rcb/poc/app/boimama/app/SplashActivity.java
@@ -2,13 +2,21 @@ package com.rcb.poc.app.boimama.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler(Looper.getMainLooper())
+                .postDelayed(() -> startActivity(new Intent(SplashActivity.this,
+                        MainActivity.class)), 3000);
     }
 }
