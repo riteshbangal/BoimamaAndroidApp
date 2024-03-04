c2d372b523d0006d32d3395d1901319d8ea520b4
Ritesh Bangal <riteshbangal@gmail.com>
2024-03-04 14:48:15 +0100

    Toggle functionality for story's image or short content in the list page

--- app/src/main/java/in/boimama/app/android/adapter/StoryAdapter.java
+++ app/src/main/java/in/boimama/app/android/adapter/StoryAdapter.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
@@ -74,6 +77,25 @@ public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.StoryViewHol
             binding.textViewStoryShortContent.setText(storyItemModel.getContent());
             binding.textViewLikesCount.setText(String.valueOf(storyItemModel.getNumberOfLikes()));
 
+            Glide.with(binding.imageViewStory.getContext())
+                    .load(storyItemModel.getStoryImageUrl())
+                    .placeholder(R.drawable.no_image)
+                    .error(R.drawable.no_image)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            Log.e("Glide", "Storey image load failed: " + e.getMessage());
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            Log.d("Glide", "Story image loaded successfully");
+                            return false;
+                        }
+                    })
+                    .into(binding.imageViewStory);
+
             Glide.with(binding.imageViewAuthorImage.getContext())
                     .load(storyItemModel.getAuthorImageUrl())
                     .placeholder(R.drawable.no_image)
@@ -81,28 +103,47 @@ public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.StoryViewHol
                     .listener(new RequestListener<Drawable>() {
                         @Override
                         public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                            Log.e("Glide", "Image load failed: " + e.getMessage());
+                            Log.e("Glide", "Author image load failed: " + e.getMessage());
                             return false;
                         }
 
                         @Override
                         public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                            Log.d("Glide", "Image loaded successfully");
+                            Log.d("Glide", "Author image loaded successfully");
                             return false;
                         }
                     })
                     .into(binding.imageViewAuthorImage);
 
+            binding.frameLayoutStoryShortContent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    toggleViews();
+                }
+            });
+
             // Set on click listener
             binding.buttonReadMore.setOnClickListener(view -> {
                 Context context = binding.getRoot().getContext();
                 Intent intent = new Intent(context, ReadStoryActivity.class);
-                intent.putExtra("storyItemModel", storyItemModel); // TODO: Need to pass story Id
+                intent.putExtra("storyItemModel", storyItemModel);
                 context.startActivity(intent);
-
-                // Show a Toast message for 2 seconds
-                //makeText(getApplicationContext(), "This feature is coming soon", Toast.LENGTH_SHORT).show();
             });
+        }
+
+        private void toggleViews() {
+            final TextView textViewStory = binding.textViewStoryShortContent;
+            final ImageView imageViewStory = binding.imageViewStory;
+
+            if (textViewStory.getVisibility() == View.VISIBLE) {
+                // If TextView is visible, hide it and show ImageView
+                textViewStory.setVisibility(View.INVISIBLE);
+                imageViewStory.setVisibility(View.VISIBLE);
+            } else {
+                // If ImageView is visible, hide it and show TextView
+                textViewStory.setVisibility(View.VISIBLE);
+                imageViewStory.setVisibility(View.INVISIBLE);
+            }
         }
     }
 }
